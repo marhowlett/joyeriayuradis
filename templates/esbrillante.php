8d8b37842e5d0a2006b8a690387906b7afdec88e
marhowlett <marimar_r2003@hotmail.com>
2020-06-17 12:50:23 -0500

    .

--- templates/esbrillante.php
+++ templates/esbrillante.php
@@ -1,5 +1,5 @@
  <section id="esbrillante" class="template-section">
  <div class="center">
- <a href="http://esbrillante.mx/">Sitio Web diseñado por EsBrillante</a>
+ <a href="http://esbrillante.mx/">Sitio Web diseñado por EsBrillante <span><a class="aviso"href="https://joyeriayuradis.com/aviso-de-privacidad-terminos-y-condiciones/">Aviso de Privacidad</a></span></a>
  </div>
 </section>
